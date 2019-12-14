969e18bb2a772ff7e0d3c2458e13aaefc81ebd9d
Cedric VERNOU <cedric.vernou@gmail.com>
2019-12-14 19:12:39 +0100

    Add the possibility to hook the configuration load

---
+++ sources/Orwel.Configuration.Hook.UnitTests/FakeConfigurationSourceProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace Orwel.Configuration.Hook.UnitTests
+{
+    class FakeConfigurationSourceProvider : ConfigurationProvider, IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return this;
+        }
+
+        public void ReloadData(IEnumerable<KeyValuePair<string, string>> data)
+        {
+            Data.Clear();
+            foreach (var d in data)
+            {
+                Data.Add(d.Key, d.Value);
+            }
+            OnReload();
+        }
+    }
+}
---
+++ sources/Orwel.Configuration.Hook/HookExtension.cs
@@ -0,0 +1,27 @@
+﻿using Orwel.Configuration.Hook;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Extensions.Configuration
+{
+    /// <summary>
+    /// Extension methods to <see cref="IConfigurationBuilder"/> to hook all registered <see cref="IConfigurationSource"/>.
+    /// </summary>
+    public static class HookExtension
+    {
+        /// <summary>
+        /// Regsiter a hook to modify configuration' values on load.
+        /// </summary>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/></param>
+        /// <param name="hook">The hook method to modify configuration's value</param>
+        /// <returns>The <see cref="IConfigurationBuilder"/></returns>
+        public static IConfigurationBuilder Hook(this IConfigurationBuilder builder, Func<KeyValuePair<string, string>, string> hook)
+        {
+            if (hook == null)
+                throw new ArgumentNullException(nameof(hook));
+            for (int i = 0; i < builder.Sources.Count; i++)
+                builder.Sources[i] = new HookConfigurationSource(builder.Sources[i], hook);
+            return builder;
+        }
+    }
+}
